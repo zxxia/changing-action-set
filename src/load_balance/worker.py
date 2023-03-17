27c385277e66764e7c5c487f776c9fd254892179
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-17 18:11:12 -0500

    Do not shuffle the job queue in workers.
    Do not know why the job queue needs to be shuffled. Fucking strange!

--- src/load_balance/worker.py
+++ src/load_balance/worker.py
@@ -1,24 +1,24 @@
-import random
+# import random
 
-def conditional_fy(l, p):
-    """Shuffle elements of a list with a given probability
+#def conditional_fy(l, p):
+#    """Shuffle elements of a list with a given probability
 
-    Args:
-        l: list
-        p: shuffle probability
-            (0: elements are never shuffled,
-             1: elements are always shuffled)
+#    Args:
+#        l: list
+#        p: shuffle probability
+#            (0: elements are never shuffled,
+#             1: elements are always shuffled)
 
-    """
-    #print("-------prob p")
-    assert 0 <= p <= 1
+#    """
+#    #print("-------prob p")
+#    assert 0 <= p <= 1
 
-    for i in range(len(l) - 1, 0, -1):
-        shuffle = random.random()
-        if shuffle < p:
-            j = random.randint(0, i - 1)
-            l[i], l[j] = l[j], l[i]
-    return l
+#    for i in range(len(l) - 1, 0, -1):
+#        shuffle = random.random()
+#        if shuffle < p:
+#            j = random.randint(0, i - 1)
+#            l[i], l[j] = l[j], l[i]
+#    return l
 
 class Worker(object):
     def __init__(self, worker_id, service_rate, wall_time, queue_shuffle_prob):
@@ -40,7 +40,7 @@ class Worker(object):
         if (self.curr_job is None or \
            self.curr_job.finish_time <= self.wall_time.curr_time) \
            and len(self.queue) > 0:
-            self.queue = conditional_fy(self.queue, p=self.queue_shuffle_prob)
+            # self.queue = conditional_fy(self.queue, p=self.queue_shuffle_prob)
             self.curr_job = self.queue.pop(0)
             duration = int(self.curr_job.size / self.service_rate)
             self.curr_job.start_time = self.wall_time.curr_time
