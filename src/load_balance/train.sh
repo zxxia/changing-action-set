2f138f401f02b4fa6d97ac38d8633f5ad51b7fd4
Zhengxu Xia <kevinxzx95@gmail.com>
2023-03-16 23:32:32 -0500

    Fix bug in training script.

--- src/load_balance/train.sh
+++ src/load_balance/train.sh
@@ -5,5 +5,5 @@
 python3 src/load_balance/run.py --num-workers 10 \
     --service-rates 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95 1.05 \
     --result-folder ./results/regular_value_network/ \
-    train
+    train \
     --model-folder ./results/parameters/regular_value_network/
