fb54a2e4ee0b68f0355eca53156a6eeeb3938250
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-16 23:14:01 -0500

    Add a LB training driver script.

---
+++ src/load_balance/train.sh
@@ -0,0 +1,9 @@
+# python src/load_balance/run.py --result-folder ./results/debug_train \
+#     train --num-ep 500 --model-folder ./models/debug_train \
+#     --model-save-interval 10
+
+python3 src/load_balance/run.py --num-workers 10 \
+    --service-rates 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95 1.05 \
+    --result-folder ./results/regular_value_network/ \
+    train
+    --model-folder ./results/parameters/regular_value_network/
