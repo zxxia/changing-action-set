67bf58b78767e15e7855494532ce42f238610610
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-22 16:17:33 -0500

    Record git branch and commit hash summary in load balance training.

--- src/load_balance/train.sh
+++ src/load_balance/train.sh
@@ -5,7 +5,14 @@
 #     train \
 #     --model-folder ./results/parameters/regular_value_network/
 
+git_summary () {
+    printf "branch: " > $1
+    git rev-parse --abbrev-ref HEAD >> $1
+    printf "commit: " >> $1
+    git rev-parse HEAD >> $1
+}
 
+git_summary ./results/parameters/regular_value_network_change_act_avail/git_summary.txt
 python3 src/load_balance/run.py --num-workers 10 \
     --service-rates 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95 1.05 \
     --result-folder ./results/regular_value_network_change_act_avail/ \
