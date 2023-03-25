Fix mask in gradient computation in load balance.

src/load_balance/input_driven_rl/actor_agent.py
@@ -167,9 +167,7 @@ class ActorAgent(object):
             self.inputs: inputs, self.mask: mask
         })
 
-    def get_gradients(self, inputs, act_vec, adv, entropy_weight):
-        # TODO: fix the mask in gradient computation
-        mask = np.ones((inputs.shape[0], 10)).astype(bool)
+    def get_gradients(self, inputs, masks, act_vec, adv, entropy_weight):
         return self.sess.run(
             [self.act_gradients, [self.adv_loss, self.entropy_loss]],
             feed_dict={
@@ -177,17 +175,18 @@ class ActorAgent(object):
                 self.act_vec: act_vec,
                 self.adv: adv,
                 self.entropy_weight: entropy_weight,
-                self.mask: mask
+                self.mask: masks
         })
 
-    def compute_gradients(self, batch_inputs, batch_act_vec, batch_adv,
+    def compute_gradients(self, batch_inputs, batch_masks, batch_act_vec, batch_adv,
                           entropy_weight):
         # stack into batch format
         inputs = np.vstack(batch_inputs)
+        masks = np.vstack(batch_masks)
         act_vec = np.vstack(batch_act_vec)
         # invoke learning model
         gradients, loss = self.get_gradients(
-            inputs, act_vec, batch_adv, entropy_weight)
+            inputs, masks, act_vec, batch_adv, entropy_weight)
         # append baseline loss
         loss.append(np.mean(batch_adv ** 2))
 

src/load_balance/input_driven_rl/train.py
@@ -106,8 +106,8 @@ def training_agent(agent_id, params_queue, reward_queue, adv_queue,
             env.change_action_availability()
 
         # set up training storage
-        batch_inputs, batch_act_vec, batch_values, batch_wall_time, batch_reward = \
-            [], [], [], [], []
+        batch_inputs, batch_masks, batch_act_vec, batch_values, \
+            batch_wall_time, batch_reward = [], [], [], [], [], []
 
         # run experiment
         state = env.observe()
@@ -131,8 +131,9 @@ def training_agent(agent_id, params_queue, reward_queue, adv_queue,
             # draw an action
             action = actor_agent.predict(inputs, mask)[0]
 
-            # store input and action
+            # store input, mask, and action
             batch_inputs.append(inputs)
+            batch_masks.append(mask)
 
             act_vec = np.zeros([1, args.num_workers])
             act_vec[0, action] = 1
@@ -187,7 +188,8 @@ def training_agent(agent_id, params_queue, reward_queue, adv_queue,
 
         # conpute gradient
         actor_gradient, loss = actor_agent.compute_gradients(
-            batch_inputs, batch_act_vec, batch_adv, entropy_weight)
+            batch_inputs, batch_masks, batch_act_vec, batch_adv,
+            entropy_weight)
         critic_gradient, _ = critic_agent.compute_gradients(
             value_inputs, batch_actual_value)
 