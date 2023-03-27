88e7e0f695cab1a2ed629396a3f350f39f64da34
Zhengxu Xia <zxxia@uchicago.edu>
2023-03-26 20:59:54 -0500

    Record uncommited changes as a patch in load balance training.

--- src/load_balance/train.sh
+++ src/load_balance/train.sh
@@ -6,16 +6,19 @@
 #     --model-folder ./results/parameters/regular_value_network/
 
 git_summary () {
-    date > $1
-    printf "branch: " >> $1
-    git rev-parse --abbrev-ref HEAD >> $1
-    printf "commit: " >> $1
-    git rev-parse HEAD >> $1
+    git_summary=$1/git_summary.txt
+    patch=$1/uncommited_changes.patch
+    date > $git_summary
+    printf "branch: " >> $git_summary
+    git rev-parse --abbrev-ref HEAD >> $git_summary
+    printf "commit: " >> $git_summary
+    git rev-parse HEAD >> $git_summary
+    git diff HEAD > $patch
 }
 
 exp_name=regular_value_network_masked
 mkdir -p ./results/parameters/${exp_name}
-git_summary ./results/parameters/${exp_name}/git_summary.txt
+git_summary ./results/parameters/${exp_name}/
 python3 src/load_balance/run.py --num-workers 10 \
     --service-rates 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95 1.05 \
     --result-folder ./results/${exp_name}/ \
